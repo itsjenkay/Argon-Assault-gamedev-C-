6165128ebf8ff0d799d79da945c0402235259be1
uduji chikordi <udujichikordi@gmail.com>
2018-10-09 15:30:34 +0100

    playing music between secen

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MusicPlayer : MonoBehaviour {
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("loadFirstScene",1f);
+	}
+	
+	// Update is called once per frame
+	void loadFirstScene()
+    {
+        
+        SceneManager.LoadScene(1);
+    }
+}
