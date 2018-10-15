7113911556ab3aaf5e69e634b7ca1c5f16458a2f
uduji chikordi <udujichikordi@gmail.com>
2018-10-15 13:02:47 +0100

    project tidying checklist

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MusicPlayer : MonoBehaviour {
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+	// Use this for initialization
+	
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("loadFirstScene", 1f);
+    }
+
+    // Update is called once per frame
+    void loadFirstScene()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+}
