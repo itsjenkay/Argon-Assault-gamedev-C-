6886c9137ac96d657046f5f878b7d4605ae9eb89
uduji chikordi <udujichikordi@gmail.com>
2018-10-16 21:35:49 +0100

    detecting particle collision

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour {
+   
+    [SerializeField] GameObject deathExplosion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnParticleCollision(GameObject other)
+    {
+        print("particle collided with enemy " + gameObject.name);
+        Destroy(gameObject);
+        print("exploded");
+        deathExplosion.SetActive(true);
+    }
+}
