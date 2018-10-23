f833b3cff18c15c28cc3733d10f651457a7dd5d7
uduji chikordi <udujichikordi@gmail.com>
2018-10-23 12:59:03 +0100

    Introducing Encapsulation

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemies : MonoBehaviour {
-
+    [SerializeField] int scorePerHit = 12;
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform Parent;
+    ScoreBoard scoreboard;
 	// Use this for initialization
 	void Start () {
+        
+        scoreboard = FindObjectOfType<ScoreBoard>();
         AddNonTriggerBoxCollider();
+
 	}
 
     private void AddNonTriggerBoxCollider()
@@ -20,11 +25,11 @@ public class Enemies : MonoBehaviour {
 
     void OnParticleCollision(GameObject other)
     {
-        GameObject FX=  Instantiate(deathFX, transform.position, Quaternion.identity);
+        scoreboard.ScoreHit(scorePerHit);
+        GameObject FX =  Instantiate(deathFX, transform.position, Quaternion.identity);
         FX.transform.parent = Parent;
        // Instantiate(deathFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
-    
        
     }
 }
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+    int score;
+    Text scoreText;
+
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+    public void ScoreHit(int scoreIncrement)
+    {
+        score = score + scoreIncrement;
+        scoreText.text = score.ToString();
+      
+    }
+
+}
